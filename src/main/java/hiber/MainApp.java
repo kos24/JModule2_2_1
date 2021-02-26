bef0d1a52172f9a82bb5285f826a2d44798207ce
Konstantin <rebrin24@gmail.com>
2021-02-26 15:31:05 +0500

    version1

--- src/main/java/hiber/MainApp.java
+++ src/main/java/hiber/MainApp.java
@@ -1,6 +1,8 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.dao.CarDao;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -14,11 +16,13 @@ public class MainApp {
             new AnnotationConfigApplicationContext(AppConfig.class);
 
       UserService userService = context.getBean(UserService.class);
+      CarDao car = context.getBean(CarDao.class);
 
       userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
       userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
       userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
       userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      car.add(new Car("Toyota",1));
 
       List<User> users = userService.listUsers();
       for (User user : users) {
@@ -28,6 +32,8 @@ public class MainApp {
          System.out.println("Email = "+user.getEmail());
          System.out.println();
       }
+
+
 
       context.close();
    }
